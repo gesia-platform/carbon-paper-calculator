686f8ca005afcf8b3a5ef8338fefd9a2e07b3cd7
winterm1 <oiool@naver.com>
2023-11-20 16:43:07 +0900

    operator location fix

--- CarbonPaperCalculator.sol
+++ CarbonPaperCalculator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../operator/IOperator.sol";
+import "./operator/IOperator.sol";
 
 contract CarbonPaperCalculator {
     // Constants
